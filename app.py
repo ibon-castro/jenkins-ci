431c1e26853bc06c27e64c635fa03f71881286d8
ibon-castro <69823509+ibon-castro@users.noreply.github.com>
2024-10-23 19:03:58 +0200

    Update app.py

--- app.py
+++ app.py
@@ -36,4 +36,4 @@ def success():
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()  # Create tables
-    app.run(host='0.0.0.0', port=5000, debug=False)
+    app.run(host='1.1.1.1', port=5000, debug=False)
